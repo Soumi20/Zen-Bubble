1767399bb339ec8cda4100dab768160d013cf221
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-05-28 22:27:54 +0530

    Add files via upload

---
+++ public/js/community.js
@@ -0,0 +1,66 @@
+// socket.io chat
+var socket = io();
+
+var form = document.getElementById('form');
+  var input = document.getElementById('input');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat message', input.value);
+      input.value = '';
+    }
+  });
+
+  socket.on('chat message', function(msg) {
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+  })
+
+  
+  // sentiment analysed community chat
+  var val = document.querySelector(".dum").innerHTML;
+
+  if(val > 0)
+  {
+    document.querySelector(".dum").innerHTML = "Your emotions have been analysed. It seems like you could share your positivity with someone in need!";
+    document.querySelector(".convo-btn").style.display = "block";
+    document.querySelector(".feel-btn").style.display = "block";
+    document.querySelector(".convo-btn").innerHTML = "Help Someone!";
+    document.querySelector(".txt-input").style.display = "none";
+  }
+
+  else if(val < 0)
+  {
+    document.querySelector(".dum").innerHTML = "Your emotions have been analysed. It seems like you'd feel better after talking to one of our kind users.";
+    document.querySelector(".convo-btn").style.display = "block";
+    document.querySelector(".feel-btn").style.display = "block";
+    document.querySelector(".convo-btn").innerHTML = "Seek Help!";
+    document.querySelector(".txt-input").style.display = "none";
+  }
+
+  else
+  {
+    document.querySelector(".txt-input").style.display = "block";
+    document.querySelector(".dum").innerHTML = "";
+    document.querySelector(".feel-btn").style.display = "none";
+    document.querySelector(".convo-btn").innerHTML = "Explore!";
+  }
+
+  function feelbox() {
+    document.querySelector(".txt-input").style.display = "block";
+    document.querySelector(".dum").style.display = "none";
+    document.querySelector(".convo-btn").style.display = "none";
+    document.querySelector(".feel-btn").style.display = "none";
+  }
+
+  function openchat() {
+    document.querySelector(".chat-container").style.display = "block";
+    document.querySelector(".convo-btn").style.display = "none";
+    document.querySelector(".dum").style.display = "none";
+    document.querySelector("footer").style.display = "none";
+  }
+
+ 
